a87ef76ec618f74905b144982056f9b29dc6fac5
Rohan Mani <64933913+rm0052@users.noreply.github.com>
2026-02-10 14:46:45 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -666,7 +666,7 @@ def get_earnings_transcript(company_name, year=None, quarter=None):
     r = requests.post(url, json=payload)
     ticker=r.json()['data'][0]['ticker']
     # Initialize the DefeatBeta client with API key
-    params = { "engine": "google", "q": question, "api_key": "1b6c33844c034b01987d113928c20e7dc77c934345ae673545479a7b77f8e7c1", "num": 30, } 
+    params = { "engine": "google", "q": query, "api_key": "1b6c33844c034b01987d113928c20e7dc77c934345ae673545479a7b77f8e7c1", "num": 10, } 
     search = GoogleSearch(params) 
     results = search.get_dict() 
     filtered_links = [result["link"] for result in results.get("organic_results", [])]
